0227c15628277129817fa54861f2191b7ab7fb03
Ryan <rfriel@rezjam.com>
2020-11-18 15:56:11 -0500

    Initial commit with slight updates.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # url_keywords
-Will take a list of keywords and check if they exist in url.
+
+url_keywords will accept two arguments(url: string, keyword: list) and output a list of matched keywords.
+
+## Description
+
+Rockerbox Full Stack Engineer - Take Home Project
+Challenge Details:
+Assume you are designing a performance application to match keywords found in a url. 
+   What data structures / technologies would you employ to ensure a speedy (sub 10ms) 
+   algorithm that is able to match substrings against the URL?
+
+(R. Friel - November 18, 2020)
+
+## Solution
+
+Quick Answer:
+    Lists & List comprehensions will be ideal here while working with Pyhon.
---
+++ main.py
@@ -0,0 +1,25 @@
+def match_keyword_in_url(url: str, keywords: list) -> list:
+    """
+    (R. Friel - November 18, 2020)
+    Adhere to single responsiblity principal
+    Perform a list comprehension on the keywords and check if a keyword is in the url.
+    """
+
+    # (R. Friel - November 18, 2020) - This is a list comprehension.
+    # Basically the for loop, conditional, and output are all in one line.
+    # Similar to this:
+    #
+    # keyword_matches = []
+    # for keyword in keywords:
+    #   if keyword in url:
+    #       keyword_matches.append(keyword)
+
+    keyword_matches: list = [ keyword for keyword in keywords if keyword in url ]
+
+    return keyword_matches
+
+
+keywords = ["www", "rockerbox", "data", "badword", "word", "tornado", "django"]
+url = "www.rockerbox.com?data=tornado"
+
+print(match_keyword_in_url(url=url,keywords=keywords))
